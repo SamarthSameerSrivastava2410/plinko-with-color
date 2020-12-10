964ebac05607da1fc8a685a68ac1151cb4932b00
SamarthSameerSrivastava2410 <72387913+SamarthSameerSrivastava2410@users.noreply.github.com>
2020-12-10 08:43:40 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,17 @@
+class Ball{
+    constructor(x,y,radius) {
+        var options = {
+            isStatic: false
+          }
+        
+      this.body = Bodies.circle(x,y,radius,options);
+      this.radius = radius;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos = this.body.position;
+      ellipseMode(CENTER);
+      fill(color(Math.round(random(0, 255)), Math.round(random(0, 255)), Math.round(random(0, 255))));
+      ellipse(pos.x, pos.y, 20, 20);
+    }
+  };
